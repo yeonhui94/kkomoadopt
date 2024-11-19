6f8837e23b6118597290a6778a816fe7f8a93fb9
kosmo <moonsangil@naver.com>
2024-11-19 17:10:56 +0900

    Button생성 및 LinkButton이벤트 삽입 , index css 루트지정

---
+++ src/main/frontend/src/components/Button.jsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+const StyledButton = styled.button`
+    padding : 12px ${props => props.horizontalPadding || "12px"};
+    color: ${props => props.bg1color ? props.bg1color : "white" };
+    background-color: ${props => props.bg1color ? "white" : props.bg1color };
+    border: 2px solid #F8755B;
+    border-radius : 10px;
+
+    &:hover {
+        color: ${props => props.bg1color ? "white" : props.bg1color};
+        background-color: ${props => props.bg1color ? props.bg1color : "white"};
+`
+
+function Button({bg1color, text, horizontalPadding}){
+    return(
+        <StyledButton
+            bg1color={bg1color}
+            
+        >    
+            {text}
+        </StyledButton>
+    )
+}
+
+export default Button;
--- src/main/frontend/src/components/LinkButton.jsx
+++ src/main/frontend/src/components/LinkButton.jsx
@@ -1,16 +1,28 @@
+import { useState } from "react";
 import styled from "styled-components";
+
 
 const StyledButton = styled.button`
     padding: 21px 29px; 
-    color : ${props => props.bgcolor ? '#ffffff' : props.bgcolor};
-    background-color : ${props => props.bgcolor ? props.bgcolor : '#ffffff'}; /* 수정된 부분 */
-    border: 3px solid #FDDCD5;
+    color : ${props => props.isClicked ? "white" : "#fffff" };
+    background-color : ${props => props.isClicked ?  props.bgcolor : "white"};
+    border: 3px solid var(--sub-color);
 `;
 
+
 function LinkButton({bgcolor, text }) {
+
+    const [isClicked, setIsClicked] = useState(false);
+
+    const handleClick = () => {
+        setIsClicked(prevState => !prevState);
+    };
+
     return (
-        <StyledButton
+        <StyledButton 
             bgcolor={bgcolor}
+            isClicked={isClicked}
+            onClick={handleClick}
         >
             {text}
         </StyledButton>

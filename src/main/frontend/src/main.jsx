9b6ebccbeb7d8dbba0304e8c734691e0cdf0301f
yeonhui <yeonhui94@icloud.com>
2024-11-19 19:13:29 +0900

    LinkButton 수정

--- src/main/frontend/src/main.jsx
+++ src/main/frontend/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Button from './components/Button.jsx'
+import LinkButton from './components/LinkButton.jsx'
 
 
 
@@ -11,6 +12,6 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
     <Button text={"로그아웃"}/>
-
+    <LinkButton text={"스크랩"}/>
   </StrictMode>,
 )

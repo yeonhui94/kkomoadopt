4e5db0b14896c8ecd1dcc4b9ce2f24209f4e66cf
yeonhui <yeonhui94@icloud.com>
2024-11-19 17:48:12 +0900

    프롭스 값 수정

--- src/main/frontend/src/components/LinkButton.jsx
+++ src/main/frontend/src/components/LinkButton.jsx
@@ -2,9 +2,14 @@ import styled from "styled-components";
 
 const StyledButton = styled.button`
     padding: 21px 29px; 
-    color : ${props => props.bgcolor ? '#ffffff' : props.bgcolor};
-    background-color : ${props => props.bgcolor ? props.bgcolor : '#ffffff'}; /* 수정된 부분 */
-    border: 3px solid #FDDCD5;
+    color : ${props => props.color || '#F8755B'};
+    background-color : ${props => props.bgcolor || '#ffffff'}; /* 수정된 부분 */
+    border: 3px solid #F8755B;
+
+    &:hover {
+        color: #ffffff;
+        background-color: #F8755B;
+    }
 `;
 
 function LinkButton({bgcolor, text }) {

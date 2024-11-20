5734c622ab7b09f96d1057611f3f1c130e49b863
KOSMO <yeonhui94@icloud.com>
2024-11-20 18:03:02 +0900

    test

--- src/main/frontend/src/App.jsx
+++ src/main/frontend/src/App.jsx
@@ -1,20 +1,13 @@
 import './App.css'
-import Card2 from './components/Card2/Card2'
-// import Header from './components/Header'
-// import Card2 from './components/Card2/Card2'
-import img1 from "./assets/CardImage/1.jpg";
+
+
+
 
 
 function App() {
 
-
   return (
-    <>
-      <Card2 imageFile={img1}></Card2>
-
-      {/* <Header></Header> */}
-      {/* <Button></Button> */}
-    </>
+    <div>dddd</div>
   )
 }
 

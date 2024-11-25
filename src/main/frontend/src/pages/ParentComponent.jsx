e0973039ce2c0f3d0b9d87f67956d7b913953496
jonggu <stu2455@naver.com>
2024-11-25 17:58:52 +0900

    Nav bar 추가

--- src/main/frontend/src/pages/ParentComponent.jsx
+++ src/main/frontend/src/pages/ParentComponent.jsx
@@ -3,7 +3,7 @@ import SubMenu from '../components/SubMenu';
 
 const ParentComponent = () => {
   const menuData = [
-    { path: '/Announcement', label: '공지사항' },
+    { path: '/Notice', label: '공지사항' },
     { path: '/find-child', label: '아이를 찾습니다' },
     { path: '/adopt-review', label: '입양후기' },
     { path: '/buy-sell', label: '사고팝니다' },

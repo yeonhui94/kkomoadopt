5bf61140c66bd7e78912d332045f43403c74ba15
KOSMO <yeonhui94@icloud.com>
2024-11-25 17:25:06 +0900

    Section3333 최종본

--- src/main/frontend/src/App.jsx
+++ src/main/frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css'
 import Card1Ex from './components/Card1/Card1Ex'
 import Header from './container/header/Header'
+// import AAA from './container/section3/AAA'
+import Section3 from './container/section3/Section3'
 import Section3333 from './container/section3/Section3333'
 import CommunityWt from './pages/CommunityWt'
 
@@ -9,9 +11,12 @@ function App() {
 
   return (
     <Section3333></Section3333>
+    // <AAA></AAA>
+    // <Section3></Section3>
     // <Card1Ex></Card1Ex>
     // <Header></Header>
     // <CommunityWt></CommunityWt>
+    // <Card1Ex></Card1Ex>
   )
 }
 

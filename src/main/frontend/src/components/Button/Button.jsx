bb6efe1216c99dd7ac58850f1442db8b2b0ab7e5
KOSMO <yeonhui94@icloud.com>
2024-11-25 10:18:12 +0900

    button props 추가

--- src/main/frontend/src/components/Button/Button.jsx
+++ src/main/frontend/src/components/Button/Button.jsx
@@ -1,39 +1,28 @@
 import styled from "styled-components";
 
 const StyledButton = styled.button`
-    padding :   ${props => props.verticalPadding || "12px"}
-                ${props => props.horizontalPadding || "26px"}; /* 두글자 기준 26px, 네글자 12px */
-    color: ${props => props.color ? props.color : 'var(--main-color)' };
-    background-color: ${props => props.backColor ? props.backColor : 'white' };
+    padding : 12px ${props => props.horizontalPadding || "26px"}; /* 두글자 기준 26px, 네글자 12px */
+    color: ${props => props.bgcolor ? props.bgcolor : 'var(--main-color)' };
+    background-color: white;
     border: 2px solid ${props => props.bg1color ? props.bg1color : 'var(--main-color)' };
     border-radius : 10px;
-    font-weight: ${props => props.fontWeight ? props.fontWeight : 'none' };
-    font-size : ${props => props.fontSize ? props.fontSize : 'none' };
-    margin-bottom : ${props => props.marginBottom ? props.marginBottom : '0px' };
-    margin-top : ${props => props.marginTop ? props.marginTop : '0px' };
 
     &:hover {
-        color: white; /* 호버 시 텍스트는 항상 흰색 */
+        color: ${props => props.hovercolor ? props.hovercolor : 'white' };/* 호버 시 텍스트는 항상 흰색 */
         background-color: ${props => props.bg1color || "var(--main-color)"}; /* 배경색 */
     }
 `;
 
-function Button({color, bg1color,backColor, text, horizontalPadding,verticalPadding, fontWeight, fontSize, marginBottom, marginTop}){
+function Button({bg1color, text, horizontalPadding,hovercolor,bgcolor}){
     return(
         <StyledButton
-            color={color}
-            bg1color={bg1color} 
-            verticalPadding={verticalPadding}
+            bg1color={bg1color}
             horizontalPadding={horizontalPadding}
-            fontWeight={fontWeight}
-            fontSize={fontSize}
-            marginBottom={marginBottom}
-            marginTop={marginTop}
-            backColor={backColor}
+            hovercolor={hovercolor}
+            bgcolor={bgcolor}
         >
             {text}
         </StyledButton>
-        
     )
 }
 

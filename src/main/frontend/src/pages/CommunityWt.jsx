053f882b5b81262795b6081a8c61da3f9741a640
소엽 <jsu4036@naver.com>
2024-11-26 11:11:03 +0900

    아이를 찾습니다

--- src/main/frontend/src/pages/CommunityWt.jsx
+++ src/main/frontend/src/pages/CommunityWt.jsx
@@ -1,23 +1,45 @@
 // import styled from "styled-components";
 import Header from "../container/header/Header";
-import Footer from "../container/Footer"
+import Footer from "../container/Footer";
 import Divider from "../components/Divider";
 import styles from "./CommunityWt.module.css";
-import InputBox from "../components/InputBox";
 
-const CommunityWt=({text = "Default Title"})=>{
-    return(
-        <div className="commwrapper">
-            <Header></Header>
-            <div className={styles.mainContainer}>
-                <h1>{text}</h1>
-                <Divider></Divider>
-                <InputBox text={"제목"} className={styles.input1}></InputBox>
-                <InputBox text={"이미지 (필수)"}className={styles.input2}></InputBox>
-                <InputBox text={"내용"} className={styles.input3}></InputBox>
-            </div>
-            <Footer></Footer>
+const CommunityWt = ({ text = "아이를 찾습니다" }) => {
+  return (
+    <div className="commwrapper">
+      <Header />
+      <div className={styles.mainContainer}>
+        <h1>{text}</h1>
+        <Divider />
+
+        {/* 등록 버튼 */}
+        <div className={styles.submitButtonContainer}>
+          <button className={styles.smallButton}>등록</button>
         </div>
-    )
-}
+
+        {/* 제목과 인풋박스를 묶은 부분 */}
+        <div className={styles.inputContainer}>
+          <h3>제목</h3>
+          <input type="text" className={styles.input} />
+        </div>
+
+        {/* 이미지와 파일첨부 버튼 */}
+        <div className={styles.inputContainer}>
+          <h3>이미지 (필수)</h3>
+          <div className={styles.fileInputContainer}>
+            <input type="text" className={styles.input} />
+            <button className={styles.button}>파일첨부</button>
+          </div>
+        </div>
+
+        {/* 내용 입력 */}
+        <div className={styles.textAreaContainer}>
+          <h3>내용</h3>
+          <textarea className={styles.textArea} />
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
 export default CommunityWt;

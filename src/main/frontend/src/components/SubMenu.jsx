887ee28a4f6b238471d225a53d51b9bf385dca2e
yeonhui <yeonhui94@icloud.com>
2024-11-26 20:32:39 +0900

    SubMenu 버튼 클릭 시 색상 고정으로 수정완료

--- src/main/frontend/src/components/SubMenu.jsx
+++ src/main/frontend/src/components/SubMenu.jsx
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import styles from '../container/SubMenu.module.css';
 
 const SubMenu = ({ menuItems }) => {
+  // 클릭된 항목을 추적하기 위한 상태 변수
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const handleClick = (index) => {
+    setActiveIndex(index); // 클릭된 항목의 인덱스를 상태로 저장
+  };
+
   return (
     <div className={styles.submenucontainer}>
       <nav className={styles.submenu}>
         <ul>
           {menuItems.map((item, index) => (
-            <li key={index}>
-              <Link to={item.path} className={styles.menuitem}>
+            <li
+              key={index}
+              className={activeIndex === index ? styles.active : ''} // 활성화된 항목에 클래스 추가
+            >
+              <Link
+                to={item.path}
+                className={`${styles.menuitem} ${activeIndex === index ? styles.active : ''}`} 
+                onClick={() => handleClick(index)} // 항목 클릭 시 상태 변경
+              >
                 {item.label}
               </Link>
             </li>

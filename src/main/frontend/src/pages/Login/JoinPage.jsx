1031ee7789e1a0d052cf6483abc4630c0df530b3
kosmo <moonsangil@naver.com>
2024-11-26 18:37:14 +0900

    로그인 페이지..

--- src/main/frontend/src/pages/Login/JoinPage.jsx
+++ src/main/frontend/src/pages/Login/JoinPage.jsx
@@ -1,17 +1,18 @@
 import styled from "styled-components";
 import Footer from "../../container/Footer";
 import Header from "../../container/header/Header";
-import LoginPageContents from "../../contents/LoginPageContents";
+
+import JoinContents from "../../contents/JoinContents";
 
 // 전체 레이아웃을 잡을 스타일 컴포넌트
 const StyledDiv = styled.div`
   display: grid;
   grid-template:
-    'header header header' 200px
+    'header' 200px
     '... ... ...' 1fr
-    '... contents ...' 600px 
+    '... contents ...' 600px
     '... ... ...' 1fr
-    'footer' 264px / 1fr 705px 1fr;
+    'footer' 264px / 1fr 607px 1fr; /* 푸터의 높이를 고정 */
   height: 110vh;  /* 화면 높이에 맞추기 위해 100vh 사용 */
   padding: 0;
   margin: 0;
@@ -26,7 +27,7 @@ const StyledHeader = styled(Header)`
   grid-area: header;
 `;
 
-const StyledContent1 = styled(LoginPageContents)`
+const StyledContent1 = styled(JoinContents)`
 
   grid-area: contents;
   margin-top : 200px;
@@ -35,12 +36,12 @@ const StyledContent1 = styled(LoginPageContents)`
 `;
 
 const StyledFooter = styled(Footer)`
-  grid-area: footer;  /* 푸터는 세 번째 줄 */
+  grid-area: footer  /* 푸터는 세 번째 줄 */
   padding-bottom: 0;
   margin-bottom: 0;
 `;
 
-function LoginPage() {
+function JoinPage() {
   return (
     <StyledDiv>
       <StyledHeader/>
@@ -50,4 +51,4 @@ function LoginPage() {
   );
 }
 
-export default LoginPage;
+export default JoinPage;

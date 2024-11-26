3f5ae9f3acd00ad6b21cbc53e8299038dd25a28e
DH <eogns6595@gmail.com>
2024-11-26 20:45:53 +0900

    Header 수정

---
+++ src/main/frontend/src/container/HeaderMain/HeaderMain.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Logo from "../../components/logo/Logo";
+import Mypageicon2 from "./MyPageIcon2";
+import styles from "../HeaderMain/HeaderMain.module.css";
+import Button from "../../components/Button/Button";
+import HeaderList from "./HeaderList";
+import Divider from "../../components/Divider";
+
+function HeaderMain() {
+  return (
+    <div className="headerGroup">
+    <div className={styles.header}>
+      <Logo />
+      <HeaderList />
+      <Mypageicon2 className="myPageicon"/>
+      <Button text={"로그아웃"}/>
+      
+    </div>
+    <Divider width={"100%"} paddingbt={"5px"} backgroundColor={"var(--line-color)"}/>
+
+
+    </div>
+  );
+}
+
+export default HeaderMain;

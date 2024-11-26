74cccd01fbc5709e8c4be404715b12da5a4ec548
yeonhui <yeonhui94@icloud.com>
2024-11-26 20:07:54 +0900

    SubMenu 버튼 클릭 시 색상 고정으로 수정완료

--- src/main/frontend/src/container/SubMenu.jsx
+++ src/main/frontend/src/container/SubMenu.jsx
@@ -1,34 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './SubMenu.css';
+import styles from "./SubMenu.module.css";
 
 const SubMenu = () => {
   return (
-    <div className="submenu-container">
-      <nav className="submenu">
+    <div className={styles.submenucontainer}>
+      <nav className={styles.submenu}>
         <ul>
           <li>
-            <Link to="/Announcement" className="menu-item">
+            <Link to="/Announcement" className={styles.menuitem}>
               공지사항
             </Link>
           </li>
           <li>
-            <Link to="/find-child" className="menu-item">
+            <Link to="/find-child" className={styles.menuitem}>
               아이를 찾습니다
             </Link>
           </li>
           <li>
-            <Link to="/adopt-review" className="menu-item">
+            <Link to="/adopt-review" className={styles.menuitem}>
               입양후기
             </Link>
           </li>
           <li>
-            <Link to="/buy-sell" className="menu-item">
+            <Link to="/buy-sell" className={styles.menuitem}>
               사고팝니다
             </Link>
           </li>
           <li>
-            <Link to="/report" className="menu-item">
+            <Link to="/report" className={styles.menuitem}>
               신고합니다
             </Link>
           </li>

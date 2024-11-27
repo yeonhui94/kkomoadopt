4c9abaa73e099762def8eca8423ca2bd8099510a
KDH <eogns6595@gmail.com>
2024-11-27 17:43:26 +0900

    기존 Header 삭제 후 업데이트

--- src/main/frontend/src/App.jsx
+++ src/main/frontend/src/App.jsx
@@ -1,39 +1,22 @@
 import React from 'react';
-import Footer from './container/Footer';
-import Header from "./container/header/Header";
-import Logo from './components/logo/Logo';
-import Profile from './components/MyPage/Profile/Profile';
-import JoinSecession from './contents/JoinSecession';
-import Divider from './components/Divider';
-import LoginPageContents from './contents/LoginPageContents';
-import AccesstionPage from './pages/Login/AccessionPage';
-import LoginPage from './pages/Login/LoginPage';
-import IdPasswordPage from './pages/Login/IdPasswordPage';
-import MemberJoinSecession from './pages/Login/MemberJoinSecession';
-import JoinContents from './contents/JoinContents';
-import JoinPage from './pages/Login/JoinPage';
-import CenterIntro from './pages/CenterIntro';
-import { createBrowserRouter, Outlet, Router, RouterProvider } from 'react-router-dom';
-import Main from './pages/Main/Main';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HeaderMain from './container/HeaderMain/HeaderMain';
+import CenterIntro from './pages/CenterIntro'; // 올바른 파일 경로로 수정
 
-import Announcement from './pages/Announcement';
-
-const router = createBrowserRouter([
-  {
-      path : "/", element : <Main/>,
-      children : [
-        { path : "/", element : <AccesstionPageContents gridArea={"section"}/>},
-        { path : "/community/announcement", element : <Announcement gridArea={"section"}/>}
-      ]
-  }
-])
-
-function App() {+
+const App = () => {
   return (
-    <div>
-      <RouterProvider router={router} />
-    </div>
+    <>
+    <Router>
+      {/* HeaderMain은 라우터 외부에 위치 */}
+      <HeaderMain />
+      {/* Route 설정 */}
+      <Routes>
+        <Route path="/center-intro" element={<CenterIntro />} />
+        
+      </Routes>
+    </Router>
+    </>
   );
-}
+};
 
 export default App;

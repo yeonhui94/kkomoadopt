29d6f0f113a41565acccfcf2595a182defb08c0f
0101 <jessica030327@gmail.com>
2024-11-28 21:21:44 +0900

    글쓰기 > 파일 첨부 넣음

--- src/main/frontend/src/App.jsx
+++ src/main/frontend/src/App.jsx
@@ -21,6 +21,24 @@ import Scrap_all from './pages/mypage/scrappg/scrap_all';
 import Scrap_dog from './pages/mypage/scrappg/scrap_dog';
 import Scrap_cat from './pages/mypage/scrappg/scrap_cat';
 import Scrap_etc from './pages/mypage/scrappg/scrap_etc ';
+import CommunityWt from './pages/community/resell/Resell_CommunityWt';
+import CommunityWt_report from './pages/community/report/Report_CommunityWt';
+import IdPasswordContents from './pages/Login/IdPasswordContents';
+import Report_Community from './pages/community/report/Report_Community';
+import Report_CommunityWt from './pages/community/report/Report_CommunityWt';
+import Find_child_community from './pages/community/find_child/Find_child_community';
+import Find_child_CommunityWt from './pages/community/find_child/Find_child_CommunityWt';
+import Resell_Community from './pages/community/resell/Resell_Community';
+import Adopt_review_Community from './pages/community/adopt_review/Adopt_review_Community';
+import Adopt_review_CommunityWt from './pages/community/adopt_review/Adopt_review_CommunityWt';
+import Resell_CommunityWt from './pages/community/resell/Resell_CommunityWt';
+import Review from './pages/Review';
+import Apply_consult from './pages/customer_service/apply_consult/Apply_consult';
+import Qna from './pages/customer_service/qna/Qna';
+import Adoption from './pages/adoption/Adoption';
+import Dog from './pages/adoption/Dog';
+import Cat from './pages/adoption/Cat';
+import Etc from './pages/adoption/Etc';
 
 
 const router = createBrowserRouter([
@@ -28,37 +46,131 @@ const router = createBrowserRouter([
       path : "/", element : <Main/>,
       children : [
         { path: "", element: <Intro/>},
+        // 아이디, 비밀번호 찾기
+        { path: "/IdPassword", element: <IdPasswordContents gridArea="section"/>},
+        //로그인
         { path: "/login", element: <LoginPageContents gridArea="section"/>},
+        //회원가입
         { path: "/join", element: <AccesstionPageContents gridArea="section"/>},
-        // { path: "adopt", element: <gridArea="section"/>}, // 입양
-        { path: "/community", element: <Community gridArea="section"/>,
+        //입양
+        // { path: "adopt", element: <Adoption gridArea="section"/>,
+        //   children : [
+        //      // 강아지
+        //      { path: "", element: <Dog gridArea="subsection"/>},
+        //      // 고양이
+        //      { path: "cat", element: <Cat gridArea="subsection"/>},
+        //      // 기타동물
+        //      { path: "etc", element: <Etc gridArea="subsection"/>},
+        //   ]
+        // },
+        //커뮤니티
+        { path: "community", element: <Community gridArea="section"/>,
+          // 커뮤니티 > 공지사항, 아이를 찾습니다, 입양 후기, 사고팝니다, 신고합니다
           children : [
+            // 공지사항
             { path: "", element: <Announcement gridArea="subsection"/>},
+            // 사고팝니다
             { path: "resell", element: <Resell gridArea="subsection"/>},
+            // 신고합니다
             // { path: "report", element: <Announcement gridArea="section"/>},
-            // { path: "adoption-review", element: <Announcement gridArea="section"/>},
+            // 입양 후기
+            { path: "adoption-review", element: <Review gridArea="section"/>},
+            // 아이를 찾습니다
             // { path: "find-child", element: <Announcement gridArea="section"/>},
           ]
         },
-        { path: "customerservice", element: <Customer_service gridArea="section"/>,
-          children : [
-            { path: "", element: <FAQ gridArea="subsection"/>},
+        // 커뮤니티 > 아이를 찾습니다 
+        { path: "commu-find_child", element: <Find_child_community gridArea="section"/>,
+          children :[
+            // 아이를 찾습니다 게시글 
+            //  { path: "", element: <CommunityWt_report gridArea="subsection"/>},
+            // 아이를 찾습니다 글쓰기
+            { path: "communitywt", element: <Find_child_CommunityWt text='신고합니다' gridArea="subsection"/>}
           ]
         },
-        { path: "/CenterIntro", element: <CenterIntro gridArea="section"/>},
-        { path: "/mypage", element: <MyPage gridArea="section"/>,
+         // 커뮤니티 > 입양 후기
+         { path: "commu-adopt_review", element: <Adopt_review_Community gridArea="section"/>,
+          children :[
+            // 입양 후기 게시글 
+            //  { path: "", element: <CommunityWt_report gridArea="subsection"/>},
+            // 입양 후기 글쓰기
+            { path: "communitywt", element: <Adopt_review_CommunityWt text='신고합니다' gridArea="subsection"/>}
+          ]
+        },
+         // 커뮤니티 > 사고팝니다
+         { path: "commu-resell", element: <Resell_Community gridArea="section"/>,
+          children :[
+            // 사고팝니다 게시글 
+            //  { path: "", element: <CommunityWt_report gridArea="subsection"/>},
+            // 사고팝니다 글쓰기
+            { path: "communitywt", element: <Resell_CommunityWt text='신고합니다' gridArea="subsection"/>}
+          ]
+        },
+         // 커뮤니티 > 신고합니다
+         { path: "commu-report", element: <Report_Community gridArea="section"/>,
+          children :[
+            // 신고합니다 게시글 
+            //  { path: "", element: < gridArea="subsection"/>},
+            // 신고합니다 글쓰기
+            { path: "communitywt", element: <Report_CommunityWt text='신고합니다' gridArea="subsection"/>}
+          ]
+        },
+        // 고객센터
+        { path: "customerservice", element: <Customer_service gridArea="section"/>,
+          // 고객센터 > FAQ , Q&A, 방문상담신청
           children : [
+            // FAQ
+            { path: "", element: <FAQ  gridArea="subsection"/>},
+            // QNA
+            // { path: "qna", element: <FAQ  gridArea="subsection"/>},
+            // 방문상담신청
+            // { path: "apply_consult", element: <FAQ  gridArea="subsection"/>},
+          ]
+        },
+        // 고객센터 > QNA
+        { path: "customer_qna", element: <Qna gridArea="section"/>,
+          children :[
+            // QNA 게시글 
+            //  { path: "", element: < gridArea="subsection"/>},
+            // QNA 글쓰기
+            // { path: "communtywt", element: <Report_CommunityWt text='신고합니다' gridArea="subsection"/>}
+          ]
+        },
+        // 고객센터 > 방문상담신청
+        { path: "customer_apply_consult", element: <Apply_consult gridArea="section"/>,
+          children :[
+            // 방문상담신청 게시글
+            //  { path: "", element: < gridArea="subsection"/>},
+            // 방문상담신청 글쓰기
+            // { path: "communtywt", element: <Report_CommunityWt text='신고합니다' gridArea="subsection"/>}
+          ]
+        },
+        // 센터 소개
+        { path: "/CenterIntro", element: <CenterIntro gridArea="section"/>},
+        // 마이페이지
+        { path: "/mypage", element: <MyPage gridArea="section"/>,
+           // 마이페이지 > 스크랩, 내가 쓴 글, 나의 댓글, 회원정보 수정, 상담신청 내역
+          children : [
+            // 스크랩 > 전체, 강아지, 고양이, 기타동물
             { path: "", element: <Scrappg gridArea="subsection" />,
               children : [
+                // 전체
                 { path: "", element: <Scrap_all gridArea="content" />},
+                // 강아지
                 { path: "scrap-dog", element: <Scrap_dog gridArea="content" />},
+                // 고양이
                 { path: "scrap-cat", element: <Scrap_cat gridArea="content" />},
+                // 기타동물
                 { path: "scrap-etc", element: <Scrap_etc gridArea="content" />},
               ]
             },
+            // 내가 쓴 글
             { path: "mypost", element: <Mypost gridArea="subsection" />},
+            // 나의 댓글
             { path: "mycomments", element: <MyComments gridArea="subsection" />},
+            // 회원정보 수정
             { path: "edit-profile", element: <EditProfile gridArea="subsection" />},
+            // 상담신청 내역
             { path: "csdatail", element: <Csdetail gridArea="subsection" />},
           ]
         },
--- src/main/frontend/src/pages/community/adopt_review/Adopt_review_CommunityWt.jsx
+++ src/main/frontend/src/pages/community/adopt_review/Adopt_review_CommunityWt.jsx
@@ -3,16 +3,14 @@ import styled from "styled-components";
 // import Footer from "../../container/Footer";
 import Divider from "../../../components/Divider";
 import styles from "../CommunityWt.module.css";
+import Uploadfile from "./Uploadfile";
+
+// 폼으로 묶어줘~~~~
 
 const Adopt_review_CommunityWt = ({gridArea}) => {
   return (
     <div 
     style={{gridArea : gridArea}}>
-        {/* 등록 버튼 */}
-        <div className={styles.submitButtonContainer}>
-          <button className={styles.smallButton}>등록</button>
-        </div>
-
         {/* 제목과 인풋박스를 묶은 부분 */}
         <div className={styles.inputContainer}>
           <h3>제목</h3>
@@ -22,16 +20,18 @@ const Adopt_review_CommunityWt = ({gridArea}) => {
         {/* 이미지와 파일첨부 버튼 */}
         <div className={styles.inputContainer}>
           <h3>이미지 (필수)</h3>
-          <div className={styles.fileInputContainer}>
-            <input type="text" className={styles.input} />
-            <button className={styles.button}>파일첨부</button>
-          </div>
+          <Uploadfile/>
         </div>
 
         {/* 내용 입력 */}
         <div className={styles.textAreaContainer}>
           <h3>내용</h3>
           <textarea className={styles.textArea} defaultValue={``} />
+        </div>
+
+        {/* 등록 버튼 */}
+        <div className={styles.submitButtonContainer}>
+          <button className={styles.smallButton}>등록</button>
         </div>
     </div>
   );

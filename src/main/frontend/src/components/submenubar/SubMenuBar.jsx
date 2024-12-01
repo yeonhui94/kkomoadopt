96e663e33dd36d07c92ffe1ed2f79f0633615eec
DH <eogns6595@gmail.com>
2024-12-01 19:54:51 +0900

    subMenuBar 및 subMenuBar.module.css 변경

--- src/main/frontend/src/components/submenubar/SubMenuBar.jsx
+++ src/main/frontend/src/components/submenubar/SubMenuBar.jsx
@@ -1,32 +1,31 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import styles from './SubMenuBar.module.css';
-import Divider from '../Divider';
 
 const SubMenuBar = ({ menuItems }) => {
+  const [selectedButton, setSelectedButton] = useState(null);
+
+  // 메뉴 클릭 시 선택된 버튼을 상태로 설정
+  const handleButtonClick = (path) => {
+    setSelectedButton(path);
+  };
+
   return (
-    <>
-    <div className={styles.subMenuBar}>
-      {menuItems.map((item) => (
-        <NavLink
-          key={item.path}
-          to={item.path}
-          className={({ isActive }) =>
-            isActive ? `${styles.menuItem} ${styles.active}` : styles.menuItem
-          }
-        > 
-          {item.name}
-        </NavLink>
-      ))}
+    <div className={styles.submenuwrap}>
+      <div className={styles.subMenuBar}>
+        {menuItems.map((item) => (
+          <Link
+            key={item.path}
+            to={item.path}
+            onClick={() => handleButtonClick(item.path)} // 버튼 클릭 시 상태 업데이트
+            className={`${styles.menuItem} ${selectedButton === item.path ? styles.active : ''}`}
+          >
+            {item.name}
+          </Link>
+        ))}
+      </div>
     </div>
-    <div className={styles.divider}>
-    <Divider display={"grid"} width={"100%"} height={"4px"} marTop={"0px"}/>
-    </div>
-    </>
   );
 };
 
 export default SubMenuBar;
-
-// /pages/community/Community
-// Community.jsx를 보고 참고하면됨! 

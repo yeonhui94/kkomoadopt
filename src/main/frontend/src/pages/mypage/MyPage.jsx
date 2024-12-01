fe648c60ab9f686dd5bd3310f8bdc508f99caaa7
yeonhui <yeonhui94@icloud.com>
2024-12-01 18:34:53 +0900

    마이페이지 - 프로필변경 링크 추가

--- src/main/frontend/src/pages/mypage/MyPage.jsx
+++ src/main/frontend/src/pages/mypage/MyPage.jsx
@@ -1,27 +1,26 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import UserNavi from '../../components/MyPage/MypageNaviBar/User/UserNavi'; // UserNavi 컴포넌트 경로에 맞게 설정
-import MyPost from '../../pages/mypage/MyPost';
-import MyComments from '../../pages/mypage/MyComments';
-import EditProfile from '../../pages/mypage/EdifProfile';
-import Csdetail from '../../pages/mypage/Csdetail';
 import Profile from "../../components/MyPage/Profile/Profile";
 import styles from "./MyPage.module.css";
-import Footer from '../../container/Footer';
 
-function MyPage({gridArea}) {
+function MyPage({ gridArea }) {
   return (
-    <div style={{gridArea : gridArea}} >
-    <div className={styles.mpWrapper}>
-    <Profile
-     name={"조랭삼"} text1={"자기소개는 부끄렁 인삼이 최고"} btnName1={"프로필 변경"} btnName2={"로그아웃"}></Profile>
-    <div className={styles.mpsmallWrapper} >
-    {/* <Router> */}
-      <UserNavi /> {/* 네비게이션 바 추가 */}
-    <Outlet/>
-    </div>  
-    </div>
-    {/* <Footer></Footer> */}
+    <div style={{ gridArea: gridArea }} >
+      <div className={styles.mpWrapper}>
+        <Profile
+          name={"조랭삼"} 
+          text1={"자기소개는 부끄렁 인삼이 최고"} 
+          btnName1={"프로필 변경"} 
+          btnName2={"로그아웃"} 
+          btnLink1="/mypage/change-profile">
+          </Profile>
+        <div className={styles.mpsmallWrapper} >
+          {/* <Router> */}
+          <UserNavi /> {/* 네비게이션 바 추가 */}
+          <Outlet />
+        </div>
+      </div>
     </div>
   );
 }

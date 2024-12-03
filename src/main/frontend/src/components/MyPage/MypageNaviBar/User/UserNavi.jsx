5c717a8bad3543d0ba28abe96e09e932a6bd5c5a
kosmo <moonsangil@naver.com>
2024-12-03 11:28:40 +0900

    유저 네비 이름도 변경

--- src/main/frontend/src/components/MyPage/MypageNaviBar/User/UserNavi.jsx
+++ src/main/frontend/src/components/MyPage/MypageNaviBar/User/UserNavi.jsx
@@ -31,7 +31,7 @@ function UserNavi() {
     { text: "스크랩", to: "/mypage" },
     { text: "내가 쓴 글", to: "/mypage/my-posts" },
     { text: "나의 댓글", to: "/mypage/my-comments" },
-    { text: "회원정보 수정", to: "/mypage/edit-profile" },
+    { text: "회원정보 수정", to: "/mypage/edit-profile1" },
     { text: "상담신청 내역", to: "/mypage/cs-detail" },
   ];
 

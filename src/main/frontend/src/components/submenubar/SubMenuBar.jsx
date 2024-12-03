ae5977534a096de434691136792423629577cca0
KOSMO <eogns6595@gmail.com>
2024-12-03 13:03:20 +0900

    커뮤니티 서브메뉴바 기능 추가 및 서브메뉴 수정

--- src/main/frontend/src/components/submenubar/SubMenuBar.jsx
+++ src/main/frontend/src/components/submenubar/SubMenuBar.jsx
@@ -1,16 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from './SubMenuBar.module.css';
 
-const SubMenuBar = ({ menuItems, onTabClick, isFilterEnabled }) => {
-  const [selectedButton, setSelectedButton] = useState(menuItems[0]?.path || menuItems[0]?.category);
-
-
-  // 메뉴 클릭 시 선택된 버튼을 상태로 설정하고 onTabClick 호출
+const SubMenuBar = ({ menuItems, selectedButton, onTabClick }) => {
   const handleButtonClick = (item) => {
-    setSelectedButton(item.path || item.category); // 링크가 있으면 링크로, 아니면 카테고리로 처리
-    if (onTabClick && !item.path) { // 링크가 없을 때만 카테고리 필터링
-      onTabClick(item.category); // 카테고리 변경을 부모 컴포넌트로 전달
+    if (onTabClick) {
+      onTabClick(item.path || item.category);
     }
   };
 
@@ -20,8 +15,8 @@ const SubMenuBar = ({ menuItems, onTabClick, isFilterEnabled }) => {
         {menuItems.map((item) => (
           <Link
             key={item.path || item.category}
-            to={item.path || "#"}  // path가 있을 경우 링크로, 아니면 "#"로
-            onClick={() => handleButtonClick(item)}  // 버튼 클릭 시 카테고리 필터링 또는 링크 이동
+            to={item.path || '#'}
+            onClick={() => handleButtonClick(item)}
             className={`${styles.menuItem} ${selectedButton === (item.path || item.category) ? styles.active : ''}`}
           >
             {item.name}

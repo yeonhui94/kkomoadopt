9f8ce803a8dd9f15e6da274a287deb545812d29b
kosmo <moonsangil@naver.com>
2024-12-05 10:11:23 +0900

    탈퇴 완료 페이지 만들기 위해 코드수정

--- src/main/frontend/src/pages/Login/JoinSecession.jsx
+++ src/main/frontend/src/pages/Login/JoinSecession.jsx
@@ -1,8 +1,6 @@
-import styled from "styled-components"
 import Button from "../../components/Button/Button";
-import logo from "../../assets/logo.svg";
 import styles from "../../pages/Login/JoinSecession.module.css";
-import { Outlet, useNavigate } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import Logo from "../../components/logo/Logo";
 
 // 텍스트 데이터 관리
@@ -14,49 +12,26 @@ const texts = {
   btnName: "메인 페이지로 이동",
 };
 
-
-const StyledLogo = styled(Logo)`
-  grid-area : logo1;
-
-`;
-
-const StyledP1 = styled.p`
-  font-size: 40px;
-  font-weight: bold;
-  margin-top: -85px;
-  
-  font-family: var(--main-font);
-`;
-
-const StyledP2 = styled.p`
-  font-size: 19px;
-  margin-bottom: 30px;
-  white-space: normal;
-  word-wrap: break-word;
-  overflow-wrap: break-word;
-  font-family: var(--main-font);
-`;
- 
-;
-
 function JoinSecession() {
-  // 텍스트를 컴포넌트 내에서 관리
- 
+
   return (
-    <div className={styles.welcomeContainer} style={{gridArea: "section"}} >
-      
+    <div className={styles.welcomeContainer} style={{ gridArea: "section" }} >
+
       <div className={styles.a1}>
-      <a href="./intro" style={{height : "100px"}} >
-          <StyledLogo width="200px"/>
-          </a>
-          </div>
-      <StyledP1 className={styles.hText1}>{texts.h1}</StyledP1>
-      <StyledP2 className={styles.p1}>{texts.text1}</StyledP2>
-      <StyledP2 className={styles.p2}>{texts.text2}</StyledP2>
-      <StyledP2 className={styles.p3}>{texts.text3}</StyledP2>
-      <div className={styles.btn1}>
-      <Button width="100%"text={texts.btnName} />
+        <a href="./intro" style={{ height: "100px" }} >
+          <Logo className={styles.logo1} width="200px" />
+        </a>
       </div>
+      
+      <h1 className={styles.hText1}>{texts.h1}</h1>
+      <p className={styles.p1}>{texts.text1}</p>
+      <p className={styles.p2}>{texts.text2}</p>
+      <p className={styles.p3}>{texts.text3}</p>
+
+      <div className={styles.btn1}>
+        <Button width="100%" text={texts.btnName} />
+      </div>
+
       <Outlet />
     </div>
   );

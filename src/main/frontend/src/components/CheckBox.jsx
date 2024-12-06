80cfe22b2e6534f3170b4185ad103993cfbb737b
KOSMO <yeonhui94@icloud.com>
2024-12-06 14:55:40 +0900

    어드민 페이지

--- src/main/frontend/src/components/CheckBox.jsx
+++ src/main/frontend/src/components/CheckBox.jsx
@@ -1,23 +1,15 @@
 import React, { useState } from "react";
 import styles from "./CheckBox.module.css";  // 모듈 CSS 파일 임포트
 
-const CheckBox = () => {
-  const [isChecked, setIsChecked] = useState(false);
-
-  const handleChange = () => {
-    setIsChecked((prev) => !prev);
-  };
-
+const CheckBox = ({ checked, onChange }) => {
   return (
     <div className={styles.checkboxContainer}>
       <input
         type="checkbox"
-        id="checkbox"
-        checked={isChecked}
-        onChange={handleChange}
+        checked={checked}
+        onChange={onChange}
         className={styles.checkbox} // 모듈 CSS 적용
       />
-   
     </div>
   );
 };

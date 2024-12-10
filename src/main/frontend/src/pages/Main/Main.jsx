db7f30af913f78393a1c0382c0e63ef0f5b0f3a0
KOSMO) <qca1313@naver.com>
2024-12-10 12:25:44 +0900

    메인수정

--- src/main/frontend/src/pages/Main/Main.jsx
+++ src/main/frontend/src/pages/Main/Main.jsx
@@ -1,7 +1,8 @@
-// src/container/Main.jsx
 import React from 'react';
 import { Outlet, useLocation } from 'react-router-dom';
 import MainHeader from '../../container/mainheader3/MainHeader';
+import Footer from '../../container/footer/Footer';
+import styles from './Main.module.css';
 
 const Main = () => {
   const location = useLocation();
@@ -9,18 +10,22 @@ const Main = () => {
   // 현재 경로가 '/' (메인 페이지)일 때는 MainHeader를 숨김
   const isMainPage = location.pathname === '/';
 
-
   const handleLogoClick = () => {
     setCurrentSection(2);  // section 2로 이동
   };
 
   return (
-    <div>
+    <div className={styles.main_grid}>
       {/* 메인 페이지일 때는 MainHeader를 숨김 */}
-      <div style={{ display: isMainPage ? 'none' : 'block' }} >
-        <MainHeader handleLogoClick={handleLogoClick}/>
+      <div style={{ display: isMainPage ? 'none' : 'block' }} className={styles.header}>
+        <MainHeader handleLogoClick={handleLogoClick} />
       </div>
-      <Outlet />
+      <div className={styles.section}>
+        <Outlet />  {/* 각 페이지의 콘텐츠가 여기로 들어옵니다 */}
+      </div>
+      <div className={styles.footer} style={{ display: isMainPage ? 'none' : 'block' }}>
+        <Footer />
+      </div>
     </div>
   );
 };

05c07ad66535c3b865eea2e671bb0397be7e9b9b
KOSMO <yeonhui94@icloud.com>
2024-12-11 15:25:16 +0900

    입양 스토어 커밋

--- src/main/frontend/src/components/header/HeaderList.jsx
+++ src/main/frontend/src/components/header/HeaderList.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom'; // Link 컴포넌트를 사용
 import styles from './MainHeader.module.css'; // MainHeader.module.css를 사용
+
 const menuItems = [
-  { name: '센터소개', path: 'http://localhost:5173/CenterIntro' },
-  { name: '입양', path: '/adoption' },
-  { name: '커뮤니티', path: 'http://localhost:5173/community' },
-  { name: '고객센터', path: 'http://localhost:5173/customerservice' },
+  { name: '센터소개', path: '/centerIntro' },
+  { name: '입양', path: '/adoption' }, // /adoption으로 수정
+  { name: '커뮤니티', path: '/community' },
+  { name: '고객센터', path: '/customerservice' },
 ];
 
 const HeaderList = ({ onItemClick }) => {
-
   const handleClick = (itemName) => {
     if (onItemClick) {
       onItemClick(itemName);
@@ -20,8 +20,7 @@ const HeaderList = ({ onItemClick }) => {
     <nav className={styles.navbar}>
       <ul className={styles.menuList}>
         {menuItems.map((item) => (
-          <li key={item.name} className={styles.menuItem}
-          onClick={() => handleClick(item.name)} >
+          <li key={item.name} className={styles.menuItem} onClick={() => handleClick(item.name)}>
             <Link to={item.path} className={styles.menuLink}>
               {item.name}
             </Link>

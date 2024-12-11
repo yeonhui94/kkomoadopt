06bf9c0f52a73d6ab3516aacb62263f540dc0bd2
KOSMO <yeonhui94@icloud.com>
2024-12-11 14:26:48 +0900

    커밋

--- src/main/frontend/src/service/apiService.js
+++ src/main/frontend/src/service/apiService.js
@@ -1,35 +1,31 @@
 import axios from "axios";
 
 // API 기본 설정
-const apiClientJson = axios.create({
+const apiClient = axios.create({
     baseURL: 'http://localhost:8080', // 백엔드 서버의 URL
     timeout: 10000,  // 요청 타임아웃 설정
     headers: { 'Content-Type': 'application/json' }
   });
   
-  const apiClientFormData = axios.create({
-    baseURL: 'http://localhost:8080', // 백엔드 서버의 URL
-    timeout: 10000,  // 요청 타임아웃 설정
-  });
 
 // 입양 게시판 관련 API
 
 // 1. 전체 입양 게시물 조회
-export const getAdoptionPosts = (pageNum) => apiClientJson.get(`/api/adopt/${pageNum}`);
+export const getAdoptionPosts = (pageNum) =>apiClient.get(`/api/adopt/${pageNum}`);
 
 // 2. 특정 입양 게시물 상세 조회
-export const getAdoptionPostDetail = (noticeUid) => apiClientJson.get(`/api/adopt/${noticeUid}`);
+export const getAdoptionPostDetail = (noticeUid) => apiClient.get(`/api/adopt/${noticeUid}`);
 
 // 3. 입양 게시물 생성
-export const createAdoptionPost = (adoptionData) => apiClientFormData.post('/api/adoption/posts', adoptionData);
+export const createAdoptionPost = (adoptionData) => apiClient.post('/api/adoption/posts', adoptionData);
 
 // 5. 입양 게시물 수정
-export const updateAdoptionPost = (noticeUid, updatedData) => apiClientFormData.patch(`/api/adoption/posts/${noticeUid}`, updatedData);
+export const updateAdoptionPost = (noticeUid, updatedData) => apiClient.patch(`/api/adoption/posts/${noticeUid}`, updatedData);
 
 // 6. 입양 게시물 삭제
-export const deleteAdoptionPost = (noticeUid) => apiClientJson.delete(`/api/adoption/posts/${noticeUid}`);
+export const deleteAdoptionPost = (noticeUid) => apiClient.delete(`/api/adoption/posts/${noticeUid}`);
 
 // // 7. 입양 게시물 이미지 업로드
 // export const uploadAdoptionPostImage = (imageData) => apiClient.post('/api/adoption/posts/upload-image', imageData);
 
-// export default apiClient;
+export default apiClient;

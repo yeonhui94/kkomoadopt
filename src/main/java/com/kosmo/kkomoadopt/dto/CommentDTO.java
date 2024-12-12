6bb314f6ad336959a7714116408e34deec0a6347
kosmo <moonsangil@naver.com>
2024-12-12 20:07:09 +0900

    Comment get부분만 하면 완료,..?!

---
+++ src/main/java/com/kosmo/kkomoadopt/dto/CommentDTO.java
@@ -0,0 +1,12 @@
+package com.kosmo.kkomoadopt.dto;
+
+import java.time.LocalDateTime;
+
+public record CommentDTO(
+        String commentId,
+        String commentContent,
+        String postUid,
+        String userId,
+        String commentDelReason
+) {
+}
